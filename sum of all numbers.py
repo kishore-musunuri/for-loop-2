73aab1eded1afb96bbfce0a391af6f32534ad12e
kishore-musunuri <kishorechowdary565@gmail.com>
2026-07-13 09:35:03 +0530

    Create sum of all numbers.py

---
+++ sum of all numbers.py
@@ -0,0 +1,9 @@
+def calculate_sum(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Example
+numbers = [10, 20, 30, 40, 50]
+print("Sum =", calculate_sum(numbers))
